b23652db083e936a5ec5c5d77b89b09b4a624646
VladyslavLata <vladlatahtc@gmail.com>
2022-06-04 17:04:54 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -15,15 +15,17 @@ function onGetOriginalImageClick(e) {
     return;
   }
   
- var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+  const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
   }
 
 function createGalleryItem({ preview, original, description }) {
-  return `<li><a class="gallery__item" href=${original}>
-  <img class="gallery__image" src=${preview} 
+  return `<li><a class="gallery__item" href='${original}'>
+  <img class="gallery__image" src='${preview}' 
   alt='${description}' /></a></li>`;
 }
 
 function createGalleryItemMarkup(galleryItems) {
   return galleryItems.map(createGalleryItem).join('');
 }
+
