16127fdb639f1e0c743054ded98e6a71a5ee67aa
VladyslavLata <vladlatahtc@gmail.com>
2022-06-04 15:32:10 +0300

    2 task completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.addEventListener('click', onGetOriginalImageClick);
+
+galleryEl.innerHTML = createGalleryItemMarkup(galleryItems);
+ 
+function onGetOriginalImageClick(e) {
+    e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  
+ var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+  }
+
+function createGalleryItem({ preview, original, description }) {
+  return `<li><a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} 
+  alt='${description}' /></a></li>`;
+}
+
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems.map(createGalleryItem).join('');
+}
